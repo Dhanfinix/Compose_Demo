95fad881957366e1f897454f9e183b43bf0091f5
Ramdhan <muhammad.ramdhan@sg-edts.com>
2025-01-31 14:15:17 +0700

    Feat: Implement Splash Screen with Logo and Updated Icons
    This commit introduces a splash screen to the app, featuring a logo and updated app icons for consistency.
    
    -   Added a `SplashScreen` composable function that displays the app logo and name for 1.5 seconds.
    -   Modified `SplashScreen` to display a logo and increase delay time.
    -   Added `composedemologo.png` as the app logo.
    -   Created `ic_launcher_background.xml` to define the background color for adaptive icons.
    -   Added app launcher foreground icons for all resolutions, `ic_launcher_foreground.webp` in `hdpi`, `mdpi`, `xhdpi`, `xxhdpi` and `xxxhdpi`.
    -   Updated `ic_launcher.xml` and `ic_launcher_round.xml` to use the new background color and foreground icons.
    - Added playstore app launcher icon `ic_launcher-playstore.png`.
    - Updated launcher icon round for all resolutions.

--- app/src/main/java/dhandev/android/composedemo/ui/screen/SplashScreen.kt
+++ app/src/main/java/dhandev/android/composedemo/ui/screen/SplashScreen.kt
@@ -1,9 +1,12 @@
 package dhandev.android.composedemo.ui.screen
 
 import android.content.Intent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,10 +14,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import dhandev.android.composedemo.R
 import dhandev.android.composedemo.constants.DeeplinksPath
@@ -37,7 +43,7 @@ fun SplashScreen(
     val host = stringResource(R.string.host)
 
     LaunchedEffect(Unit) {
-        delay(1000)
+        delay(1500)
         handleDeeplink(navController, intent, schemes, host)
     }
     Surface(modifier.fillMaxSize()) {
@@ -46,6 +52,13 @@ fun SplashScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            Image(
+                painter = painterResource(R.drawable.composedemologo),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .clip(CircleShape)
+            )
             Text(
                 text = "Compose Demo",
                 style = MaterialTheme.typography.headlineLarge
