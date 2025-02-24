7ac4675efdf3db7a099b0f8b86136e088c81dbe0
Ramdhan <muhammad.ramdhan@sg-edts.com>
2025-02-24 14:05:19 +0700

    Change: update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "edts.android.composedemo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
